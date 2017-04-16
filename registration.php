00e33dada056af7ce7795e49bb8f0bf9feb3aa1b
Ethan Grant <ethangrant93@googlemail.com>
2017-04-16 20:53:35 +0100

    Initial commit

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Ethangrant_Flipclock',
+    __DIR__
+);
---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+var config = {
+    map: {
+        '*': {
+            'flipclock':'Ethangrant_Flipclock/js/flipclock.min',
+        }
+    },
+    shim: {
+        'flipclock': {
+            'deps':['jquery']
+        }
+    }
+};
